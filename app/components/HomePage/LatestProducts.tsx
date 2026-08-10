7da24953675c461b684b61d8f481b7132be7101a
f-malekan <fateme.malekan96@gmail.com>
2026-08-10 16:23:02 +0330

    fix latest products link

--- app/components/HomePage/LatestProducts.tsx
+++ app/components/HomePage/LatestProducts.tsx
@@ -3,6 +3,7 @@ import ProductCard from "../Product/ProductCard";
 import ErrorState from "../CommonComponents/ErrorState";
 import CommonSectionHeader from "../CommonComponents/CommonSectionHeader";
 import { BaseSlider } from "../BaseComponents/BaseSlider";
+import Link from "next/link";
 
 const LatestProducts = async () => {
   const { data: products, success, message } = await getLatestProducts();
@@ -13,9 +14,11 @@ const LatestProducts = async () => {
     <section className="p-2 md:p-12">
       <CommonSectionHeader title="جدیدترین محصولات" href="/products" />
       <div className="container mx-auto mt-10">
-        <BaseSlider slidesPerView={{ sm: 2.1, md: 3.3, lg:4.1 }}>
+        <BaseSlider slidesPerView={{ sm: 2.1, md: 3.3, lg: 4.1 }}>
           {products?.map((product) => (
-            <ProductCard key={product.id} product={product} />
+            <Link key={product.id} href={`/products/${product.id}`}>
+              <ProductCard key={product.id} product={product} />
+            </Link>
           ))}
         </BaseSlider>
       </div>
